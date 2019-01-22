d595f49e00659ded077b2cdce60466398317d5d8
blackwell <blackwellsmithiv@gmail.com>
2019-01-22 18:37:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,10 @@ def long_planeteer_calls(long_list)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese_array)
+  i = 0 
+  while i < cheese_array.length
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  end
 end
