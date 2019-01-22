cd64ab463f4ebb94e6b70a08f56d162cc5bc9a09
blackwell <blackwellsmithiv@gmail.com>
2019-01-22 18:33:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(long_list)
-  if long_list.find {|i| i.length >= 4 }
+  if long_list.find {|i| i.length > 4 }
     true 
   else
     false
