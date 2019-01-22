74cd59e996dcff8c1ac4147adadad7f2cb9a0b0f
blackwell <blackwellsmithiv@gmail.com>
2019-01-22 18:20:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ end
 def long_planeteer_calls(long_list)
   if long_list.find {|i| i.length >= 4 }
     true 
-  elsif long_list.include {|i| i.length > 4}
-    true
+  else 
+    false
   end
 end
 
